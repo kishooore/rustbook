4da2eb45012f7884ae9d2814be879b4c6bd48815
Kishore Garapati <kishore@Kishores-MacBook-Pro.local>
2025-03-26 21:05:10 +0530

    add s3 file downloader implementation

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,11 @@ async fn main() -> Result<()> {
     info!("Cloud file downloader starting...");
 
     let downloader = S3Downloader::new().await?;
+
+    downloader.download_file(
+            "kishore-cloudera-dbus-dev",
+            "CmlWorkloads/037c789f-4033-4633-8038-b1868bb114f6",
+            "/Users/kishore/037c789f-4033-4633-8038-b1868bb114f6")
+        .await?;
     Ok(())
 }
